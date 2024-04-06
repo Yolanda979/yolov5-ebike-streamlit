dd0932ec8aef850e4f4b71f25706d5f916fa58f3
Yolanda979 <148202432+Yolanda979@users.noreply.github.com>
2024-04-06 18:21:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ import os
 import time
 import ultralytics
 
+__import__('pysqlite3')
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 st.set_page_config(layout="wide")
 
 cfg_model_path = 'models/best.pt'
